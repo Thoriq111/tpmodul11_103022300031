9efae63ca538ca9dc58f0a8bf25794608144a39c
Thoriq111 <thoriqmarcello2146@gmail.com>
2025-05-04 20:25:11 +0700

    Menambahkan Libraries Aljabar

---
+++ Tpmodul11_103022300031/AljabarLibraries/PersamaanKuadrat.cs
@@ -0,0 +1,35 @@
+﻿namespace Tpmodul11_103022300031.AljabarLibraries
+{
+    namespace AljabarLibraries
+    {
+        public class PersamaanKuadrat
+        {
+            public static double[] AkarPersamaanKuadrat(double[] persamaan)
+            {
+                double a = persamaan[0];
+                double b = persamaan[1];
+                double c = persamaan[2];
+                double diskriminan = b * b - 4 * a * c;
+
+                if (diskriminan < 0)
+                    return new double[] { }; // tidak ada akar real
+
+                double akar1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+                double akar2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+                return new double[] { akar1, akar2 };
+            }
+
+            public static double[] HasilKuadrat(double[] persamaan)
+            {
+                double a = persamaan[0];
+                double b = persamaan[1];
+
+                double a_kuadrat = a * a;
+                double dua_ab = 2 * a * b;
+                double b_kuadrat = b * b;
+
+                return new double[] { a_kuadrat, dua_ab, b_kuadrat };
+            }
+        }
+    }
+}
---
+++ Tpmodul11_103022300031/Tpmodul11App/program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tpmodul11_103022300031.Tpmodul11App
+{
+    using AljabarLibraries;
+    using Tpmodul11_103022300031.AljabarLibraries.AljabarLibraries;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] persamaan1 = { 1, -3, -10 };
+            double[] akar = PersamaanKuadrat.AkarPersamaanKuadrat(persamaan1);
+            Console.WriteLine("Akar dari x^2 - 3x - 10 = 0:");
+            foreach (var item in akar)
+                Console.WriteLine(item);
+
+            double[] persamaan2 = { 2, -3 };
+            double[] hasilKuadrat = PersamaanKuadrat.HasilKuadrat(persamaan2);
+            Console.WriteLine("Hasil kuadrat dari (2x - 3)^2:");
+            foreach (var item in hasilKuadrat)
+                Console.WriteLine(item);
+        }
+    }
+}
